f8ae0fbe9efeefa218047ce9111a708a0dd48f82
Elijah Heath <ejheath8888@gmail.com>
2022-02-24 10:19:50 -0500

    temp conversions using static

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(44);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(44);
+
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
+        }
+    }
+}
